9cff7699cb28319ffde4cfac2a7cf64de44934a0
Yusuf <ybasori@gmail.com>
2020-09-05 19:55:21 +0700

    12.creating-api

--- server/routes/web.js
+++ server/routes/web.js
@@ -4,8 +4,8 @@ import Blog from "../controllers/BlogController";
 const web = (app) => {
   app.get("/blog", Blog.index);
   app.get("/blog/:id", Blog.show);
-  app.post("/blog", multer().none(), Blog.store);
-  app.put("/blog/:id", multer().none(), Blog.update);
+  app.post("/blog/create", multer().none(), Blog.store);
+  app.put("/blog/:id/edit", multer().none(), Blog.update);
   app.delete("/blog/:id", multer().none(), Blog.delete);
   return app;
 };
