efe7adbcdceca3870cfe321ff774ff7529ba46fe
Yusuf <ybasori@gmail.com>
2020-09-13 18:36:47 +0700

    perbaikan dikit

---
+++ index.js
@@ -0,0 +1,9 @@
+require("regenerator-runtime/runtime");
+require("ignore-styles");
+
+require("@babel/register")({
+  ignore: [/(node_modules)/],
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+});
+
+require("./server");
